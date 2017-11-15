c9a941b407c04c82afc6d30585b1439b38db6338
Clayton Massie <claytonmassie@Claytons-MacBook-Pro.local>
2017-11-15 10:01:52 -0600

    worked on ticket on one and two

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,24 @@
 class ProductsController < ApplicationController
+
+    # makes new instance for new products that is referd to in the view.
+    def new 
+        @product = Product.new
+    end 
+    # "create" lets you take in new params and save them to database.
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+        redirect_to @product
+        # "else" makes sure product params are satisfied, will not let you save invalid data to database.
+        else 
+            render 'new'
+        end
+    end 
+
+    # makes sure the product_params are not accesable to user.
+    private
+    def product_params
+        params.require(:product).permit(:title, :category, :type, :price, :delivery, :city)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171114200454) do
+ActiveRecord::Schema.define(version: 20171114200202) do
 
   create_table "order_products", force: :cascade do |t|
     t.integer "product_id"
