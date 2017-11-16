c1824a0aee2cae2c4c5e8a81fcbee5c97762ab8d
Jasmine Quach <xjasminequach@gmail.com>
2017-11-16 15:26:17 -0600

    Fix broken Sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create( user_params
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
@@ -19,6 +18,6 @@ class UsersController < ApplicationController
   # Be sure to update your create() and update() controller methods.
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :product_image
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
