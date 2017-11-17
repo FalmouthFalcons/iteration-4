e2bddb4f351a531d56e9d11227a89aba3b282bf5
Jim Vickery <jimrv8@yahoo.com>
2017-11-17 15:36:07 -0600

    saving for pulldown

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -3,6 +3,7 @@ class ProductTypesController < ApplicationController
     def index
         # gets number of items in each category for show page
         @products_count = Product.group(:category).count(:category)
+        
     end
 
 end
