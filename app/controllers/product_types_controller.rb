1ba2dc0ce84ebeed410b3e00802d8203721708ac
Malcolm Ford <malcolmf38@gmail.com>
2017-11-20 14:24:12 -0600

    Working

--- app/controllers/product_types_controller.rb
+++ app/controllers/product_types_controller.rb
@@ -5,6 +5,7 @@ class ProductTypesController < ApplicationController
         # @products_count = Product.group(:category).count(:category)
         # This might work
         @products_count = ProductType.all
+        @product_number = ProductType.count(:category)
         # Model.distinct.pluck(:rating)
         
     end
