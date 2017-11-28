d79ae9d577ec5cd0d1c419cf986026006d9b1947
Adam D. White <adamwhite82@gmail.com>
2017-11-28 16:26:36 -0600

    completing an order!!

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,7 +16,7 @@ class OrdersController < ApplicationController
             new_order_product.product_id = product_params[:id]
             new_order_product.order_id = @order[0].id
             new_order_product.save
-            redirect_to @order[0]
+            render 'index'    
         else
             # identifies an existing order - push a new product to an old order 
            new_order = Order.new
@@ -26,18 +26,19 @@ class OrdersController < ApplicationController
            new_order_product.product_id = product_params[:id]
            new_order_product.order_id = new_order.id
            new_order_product.save
-           redirect_to 'new'
+           render 'index'
         end
   
         # takes user to the view of Shopping Cart
     end
 
-    def show
+    def index 
 
         @user = User.find(session[:user_id]) 
         puts @user
         #added order of shopping cart so that last item added is at top
-        @order_product = OrderProduct.where(order_id: params[:id]).order('id DESC')
+        @order = Order.find_by(:customer_id => @user.id, :payment_type_id => nil)
+        @order_product = OrderProduct.where(order_id: @order.id).order('id DESC')
         
         @final = []
 
@@ -61,13 +62,19 @@ class OrdersController < ApplicationController
     end
 
     # order confirmation page with details
-    def complete_order
-        @payment_types = PaymentType.where(customer_id: session[:user_id] )
+    def update
+        @payment_types = PaymentType.find(params[:payment_type_id])
         @user = User.find(session[:user_id])
-        @order = Order.where(customer_id: @user.id, payment_type_id: nil)
-        
+        @order = Order.find_by(customer_id: @user.id, payment_type_id: nil)        
+        # setting pmt type id 
+        @order.payment_type_id = @payment_types.id 
+        @order.save
+        redirect_to '/complete_order'
     end
 
+    def complete_order
+        @user = User.find(session[:user_id])
+    end
     
     private
     def product_params
